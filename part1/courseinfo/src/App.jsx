cd996c1124b1b6564d6782dd9cdd3f1d2249f81a
Shahram Barai <barais1@aalto.fi>
2024-09-27 12:41:28 +0300

    Add exercises 1.3-1.5 solutions

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,48 +1,48 @@
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+  return <h1>{props.course}</h1>;
+};
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
 
 const Content = (props) => {
   return (
     <>
-      {props.assignments.map((assignment, index) => (
-        <Part key={index} part={assignment.part} exercises={assignment.exercises} />
+      {props.parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
       ))}
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   let total = 0;
-  props.assignments.forEach(assignment => total += assignment.exercises); 
-  return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
+  props.parts.forEach((part) => (total += part.exercises));
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const assignments = [
-    {part: 'Fundamentals of React', exercises: 10},
-    {part: 'Using props to pass data', exercises: 7},
-    {part: 'State of a component', exercises: 14}
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content assignments={assignments} />
-      <Total assignments={assignments} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
