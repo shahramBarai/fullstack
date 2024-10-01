f7dd284cbff14d79b6f567065710049c16fdab0b
Shahram Barai <barais1@aalto.fi>
2024-10-01 19:42:31 +0300

    Add solution for 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const onNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={onNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddPerson}>
+            Add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
