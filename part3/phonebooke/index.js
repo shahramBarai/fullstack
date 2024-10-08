38038bb449a989e61df984877ae0aca1d4b300be
Shahram Barai <shahram.barai@gmail.com>
2024-10-08 16:19:39 +0300

    Add solutions for 3.5-3.6

--- part3/phonebooke/index.js
+++ part3/phonebooke/index.js
@@ -55,6 +55,23 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name or number missing" });
+  }
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+  const person = {
+    id: `${Math.floor(Math.random() * 1000)}`,
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
 app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
